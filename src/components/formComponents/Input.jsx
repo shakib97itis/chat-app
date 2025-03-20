3912eba61a1cd10193214d07a2f8bf4e3991fa3f
Shakibul Islam <8080.shakib@gmail.com>
2025-03-20 12:42:30 +0600

    update Input.jsx for using diffrent style varient

--- src/components/formComponents/Input.jsx
+++ src/components/formComponents/Input.jsx
@@ -1,23 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Input({ type, label, placeholder, name, id, className }) {
+const varient = {
+  one: "mb-7",
+  two: "mb-3.5",
+};
+
+function Input({ type, label, placeholder, name, id, className, varient }) {
   return (
-    <div className={`relative mt-3.5 flex flex-col ${className}`}>
-      <label
-        className="absolute top-0 left-8 -translate-y-1/2 bg-white px-2.5 font-serif text-[14px] font-semibold text-space"
-        htmlFor="emailId"
-      >
-        {label}
-      </label>
-      <input
-        className={`rounded-[8.6px] border-[1.7px] border-space px-10 py-5 font-serif text-[20px] font-semibold text-space focus:outline-none`}
-        type={type}
-        name={name}
-        id={id}
-        placeholder={placeholder}
-      />
-    </div>
+    <>
+      {varient === "one" && (
+        <div className={`relative mt-3.5 flex flex-col ${className}`}>
+          <label
+            className="absolute top-0 left-8 -translate-y-1/2 bg-white px-2.5 font-serif text-[14px] font-semibold text-space"
+            htmlFor="emailId"
+          >
+            {label}
+          </label>
+          <input
+            className={`rounded-[8.6px] border-[1.7px] border-space px-10 py-5 font-serif text-[20px] font-semibold text-space focus:outline-none`}
+            type={type}
+            name={name}
+            id={id}
+            placeholder={placeholder}
+          />
+        </div>
+      )}
+
+      {varient === "two" && (
+        <div className={`flex flex-col lg:min-w-[372px] ${className}`}>
+          <label
+            className="font-sans text-sm font-normal text-space/50"
+            htmlFor="emailId"
+          >
+            {label}
+          </label>
+          <input
+            className={`border-b-[1.7px] border-space px-1 py-4 font-sans text-xl font-semibold text-space focus:outline-none`}
+            type={type}
+            name={name}
+            id={id}
+            placeholder={placeholder}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
@@ -28,6 +55,7 @@ Input.defaultProps = {
   name: "",
   id: "",
   className: "",
+  varient: "one",
 };
 
 Input.propTypes = {
@@ -37,6 +65,7 @@ Input.propTypes = {
   name: PropTypes.string,
   id: PropTypes.string,
   className: PropTypes.string,
+  varient: PropTypes.string.isRequired,
 };
 
 export default Input;
