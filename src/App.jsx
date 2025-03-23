cdac73dcd8ea13d477cde0908f5e8dad2456aa5c
Shakibul Islam <8080.shakib@gmail.com>
2025-03-24 00:32:05 +0600

    add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Registration from "./pages/ragistration/Registration";
 import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router";
+import firebaseApp from "./utils/firebase.config.js";
+import { getAuth } from "firebase/auth";
+const auth = getAuth(firebaseApp);
+
 
 function App() {
   return (
