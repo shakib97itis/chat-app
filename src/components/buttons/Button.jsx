2eb48a2d4ea1af55fc1e88e8eb95ad75a39e0ecb
Shakibul Islam <8080.shakib@gmail.com>
2025-03-23 15:48:11 +0600

    update Button.jsx and LoginWithButton.jsx

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Button({ children, className }) {
   return (
     <button
-      className={`w-full cursor-pointer hover:scale-105 transition-transform rounded-full bg-space py-4 text-white ${className} `}
+      className={`w-full cursor-pointer rounded-full bg-deep-sea py-4 font-serif text-xl font-semibold text-white transition hover:scale-105 ${className} `}
     >
       {children}
     </button>
--- src/components/buttons/LoginWithButton.jsx
+++ src/components/buttons/LoginWithButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
-function LoginWithButton() {
+function LoginWithButton({ className, text, FaIcon }) {
   return (
-    <div>LoginWithButton</div>
-  )
+    <button className="flex items-center gap-2.5 rounded-lg border border-deep-sea px-7 py-5 pe-16 font-sans font-semibold text-deep-sea">
+      <FaIcon className="align-middle text-3xl" />
+      <span>{text}</span>
+    </button>
+  );
 }
 
-export default LoginWithButton
+LoginWithButton.propTypes = {
+  className: PropTypes.string,
+  Faicon: PropTypes.element,
+}
+
+export default LoginWithButton;
