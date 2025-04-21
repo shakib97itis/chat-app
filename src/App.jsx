bf7c94a4ff32b73d5baa871a9288e7627f0b0168
Shakibul Islam <shakib97itis@gmail.com>
2025-04-21 16:42:29 +0600

    refactor(App): remove unused Login route from application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Registration from "./pages/ragistration/Registration";
-import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Registration from "./pages/ragistration/Registration";
 import Home from "./pages/home/Home";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
