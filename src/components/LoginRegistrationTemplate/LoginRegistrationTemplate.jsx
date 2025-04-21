c42a428c3e6cd6906267175ba188259787ac2793
Shakibul Islam <shakib97itis@gmail.com>
2025-04-21 16:42:46 +0600

    feat(LoginRegistrationTemplate): add new component for login and registration layout

---
+++ src/components/LoginRegistrationTemplate/LoginRegistrationTemplate.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const LoginRegistrationTemplate = ({ children, bannerImg }) => {
+  return (
+    <section className="">
+      <div className="flex lg:p-0 min-h-screen justify-between p-4 lg:h-screen">
+        {/* LEFT SIDE */}
+        <div className="flex w-full items-center justify-center lg:w-3/5 lg:justify-end lg:pe-[68px]">
+          {children}
+        </div>
+        {/* RIGHT SIDE */}
+        <div className="hidden h-full w-2/5 lg:block">
+          <img
+            className="h-full w-full object-cover"
+            src={bannerImg.src}
+            alt={bannerImg.alt}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+LoginRegistrationTemplate.propTypes = {
+  children: PropTypes.node.isRequired,
+  bannerImg: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+  }),
+};
+
+export default LoginRegistrationTemplate;
