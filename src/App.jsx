79f5f95a4a64db177b62e090909d5791af79233a
Shakibul Islam <shakib97itis@gmail.com>
2025-04-21 18:12:15 +0600

    feat(App): add login route to the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Registration from "./pages/ragistration/Registration";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
