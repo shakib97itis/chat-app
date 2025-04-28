c033287febdbe1262c415852984d866001508472
Shakibul Islam <shakib97itis@gmail.com>
2025-04-28 16:48:26 +0600

    feat(Container): enhance layout and styling of the container component

--- src/components/common/container/container.jsx
+++ src/components/common/container/container.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const container = ({ children, className }) => {
-  return <div className={`container mx-auto ${className}`}>{children}</div>;
+  return (
+    <div className={`mx-auto max-w-[1440px] flex min-h-dvh sm:p-2.5 ${className}`}>
+      <div className="w-full">{children}</div>
+    </div>
+  );
 };
 
 export default container;
