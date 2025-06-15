94ce9f9edcf32c089e6376b4c762770666c728a4
Shakibul Islam <shakib97itis@gmail.com>
2025-06-16 01:42:28 +0600

    feat(Message): refactor Message component to use itemsList for DashboardCard props and remove unused imports

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,25 +1,52 @@
-import React from "react";
-import Container from "../../components/common/container/container";
+import Container from "../../components/common/container/Container";
 import Navigation from "../../components/navigation/Navigation";
 import Search from "../../components/common/search/Search";
 import DashboardCard from "../../components/DashboardCard/DashboardCard";
 import Chat from "../../components/chat/Chat";
-import Icons from "../../Icons";
+
+const itemsList = [
+  {
+    id: 1, // unique id of the card
+    title: "Card Title 1",
+    description: "Description 1",
+    imageUrl: "/profile-img/person-1.png",
+    text: "Button 1",
+  },
+  {
+    id: 2, // unique id of the card
+    title: "Card Title 2",
+    description: "Description 2",
+    imageUrl: "/profile-img/person-2.png",
+    text: "Button 2",
+  },
+  {
+    id: 3, // unique id of the card
+    title: "Card Title 3",
+    description: "Description 3",
+    imageUrl: "/profile-img/person-3.png",
+    text: "Button 3",
+  },
+  // {
+  //   id: 4, // unique id of the card
+  //   title: "Card Title 4",
+  //   description: "Description 4",
+  //   imageUrl: "/profile-img/person-4.png",
+  //   text: "Button 4",
+  // },
+];
 
 const Message = () => {
   return (
     <Container>
       <div className="flex h-full gap-[20px]">
-        {/* will be deleted soon */}
-        {/* <Icons /> */}
         {/* NAVIGATION BAR */}
         <Navigation />
+
         {/* Dashboard content */}
-        <main className="grid w-full  grid-cols-3 gap-[20px]">
+        <main className="grid w-full grid-cols-3 gap-[20px]">
           {/* Left Side */}
           <div className="col-span-1">
-            <Search />
-            <DashboardCard cardTitle={"Messages"} />
+            <DashboardCard searchVisible={true} cardTitle={"Messages"} items={itemsList} />
           </div>
           {/* Right Side */}
           <div className="col-span-2">
