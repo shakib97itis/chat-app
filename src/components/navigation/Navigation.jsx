5a8156910a207fb45ee6ba208486c31fa3be400b
Shakibul Islam <shakib97itis@gmail.com>
2025-06-16 01:42:10 +0600

    feat(Navigation): update NavigationItem to accept URL prop and refactor navigation structure

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import { MdOutlineHome } from "react-icons/md";
 import NavigationItem from "./NavigationItem";
+import { MdOutlineHome } from "react-icons/md";
 import { LuMessageCircleMore } from "react-icons/lu";
 import { MdNotifications } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
 import { ImExit } from "react-icons/im";
 
 const Navigation = () => {
+  /**
+   * TODO: write navigation logic here.
+   * TODO: add logout logic later.
+   */
   return (
     <div className="flex flex-col items-center rounded-2xl bg-primary py-[20px]">
       {/* PROFILE IMAGE */}
-      <div className="mx-[10px] sm:mx-[25px] mb-[10px] h-[50px] w-[50px] sm:h-[100px] sm:w-[100px] overflow-hidden rounded-full">
+      <div className="mx-[10px] mb-[10px] h-[50px] w-[50px] overflow-hidden rounded-full sm:mx-[25px] sm:h-[100px] sm:w-[100px]">
         <img
           className="h-full w-full object-cover"
           src="/public/profile-img/profile-6.png"
@@ -19,16 +22,16 @@ const Navigation = () => {
       </div>
 
       {/* USER NAME */}
-      <div className="text-center font-tertiary text-lg sm:text-2xl font-bold text-neutral-01 mx-[5px] mb-[10px]">
+      <div className="mx-[5px] mb-[10px] text-center font-tertiary text-lg font-bold text-neutral-01 sm:text-2xl">
         John Doe
       </div>
 
       {/* NAVIGATION ITEMS */}
       <div className="w-full sm:ps-[20px]">
-        <NavigationItem NavIcon={MdOutlineHome} />
-        <NavigationItem NavIcon={LuMessageCircleMore} />
-        <NavigationItem NavIcon={MdNotifications} />
-        <NavigationItem NavIcon={IoMdSettings} />
+        <NavigationItem NavIcon={MdOutlineHome} url="/" />
+        <NavigationItem NavIcon={LuMessageCircleMore} url="/message" />
+        <NavigationItem NavIcon={MdNotifications} url="/notification" />
+        <NavigationItem NavIcon={IoMdSettings} url="/settings" />
       </div>
 
       {/* LOGOUT */}
--- src/components/navigation/NavigationItem.jsx
+++ src/components/navigation/NavigationItem.jsx
@@ -1,21 +1,26 @@
-import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router";
 
-const NavigationItem = ({ NavIcon }) => {
+
+
+const NavigationItem = ({ NavIcon, url }) => {
   return (
-    <div className="group relative flex cursor-pointer items-center justify-center rounded-s-2xl bg-transparent py-[15px] text-neutral-01 transition hover:bg-neutral-01 hover:text-primary mb-[10px]">
+    <Link
+      to={url}
+      className="group relative mb-[10px] flex cursor-pointer items-center justify-center rounded-s-2xl bg-transparent py-[15px] text-neutral-01 transition hover:bg-neutral-01 hover:text-primary"
+    >
       {/* Icon */}
       <div className="relative sm:left-[-10px]">
         {NavIcon && <NavIcon className="text-[32px] sm:text-[46px]" />}
       </div>
       {/* bar */}
       <div className="absolute right-0 h-full w-[8px] rounded-s-2xl bg-secondary opacity-0 transition group-hover:opacity-100"></div>
-    </div>
+    </Link>
   );
 };
 
-NavigationItem.propTypes = {
-  icon: PropTypes.element.isRequired,
-};
+/**
+ * TODO: Add propsTypes later.
+ */
 
 export default NavigationItem;
